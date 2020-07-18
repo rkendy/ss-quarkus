b8e98f40f6b1cce8b068f78ece70b82bef2bbd3b
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-07-18 15:26:15 -0300

    Rename Setor test method name

--- src/test/java/br/uff/ihs/model/SetorTest.java
+++ src/test/java/br/uff/ihs/model/SetorTest.java
@@ -13,17 +13,16 @@ import io.quarkus.test.junit.QuarkusTest;
 public class SetorTest {
 
     @Test
-    public void test() {
+    public void givenSetor_whenCount_thenSuccess() {
         assertEquals(3, Setor.count());
     }
 
     // @Test
-    // // @Transactional
-    // public void test2() {
+    // @Transactional
+    // public void givenNewSetor_whenPersist_thenSuccess() {
     // Setor s =
     // Setor.builder().ativo(true).codigo(Setor.Codigo.ALMOXARIFADO).nome("Nome").build();
     // s.persist();
     // assertTrue(s.isPersistent());
-
     // }
 }
